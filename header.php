4b11dcc677370263bf2ac59db64ba46621b9eaab
Joseph Dagunan <joseph.dagunan@dcmail.ca>
2016-09-26 17:29:21 -0400

    deleted class files

--- header.php
+++ header.php
@@ -7,12 +7,10 @@
 
 // Include Files
 require_once('includes/constants.php');
-require_once('includes/functions.php');
 require_once('includes/db.php');
+require_once('includes/functions.php');
 
 
-// Models
-require_once('model/users.class.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
--- includes/functions.php
+++ includes/functions.php
@@ -37,3 +37,108 @@ function encryptPassword($password){
 
     return $encryptedPassword;
 }
+
+class Users
+{
+    private $userId;
+    private $password;
+    private $userType;
+    private $emailAddress;
+    private $enrolDate;
+    private $lastAccess;
+
+    private function getUserId() {
+        return $this->userId;
+    }
+
+    private function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    private function getPassword() {
+        return $this->password;
+    }
+
+    private function setPassword($password) {
+        $this->password = $password;
+    }
+
+    private function getUserType() {
+        return $this->userType;
+    }
+
+    private function setUserType($userType) {
+        $this->userType = $userType;
+    }
+
+    private function getEmailAddress() {
+        return $this->emailAddress;
+    }
+
+    private function setEmailAddress($emailAddress) {
+        $this->emailAddress = $emailAddress;
+    }
+
+    private function getEnroldate() {
+        return $this->enrolDate;
+    }
+
+    private function setEnroldate($enrolDate) {
+        $this->enrolDate = $enrolDate;
+    }
+
+    private function getLastAccess() {
+        return $this->lastAccess;
+    }
+
+    private function setLastAccess($lastAccess) {
+        $this->lastAccess = $lastAccess;
+    }
+
+
+
+    public function GetUser($email){
+        $userData = array();
+
+        $query = "SELECT * FROM users WHERE email='$email'";
+        $result = pg_query(db_connect(), $query);
+        $records = pg_num_rows($result);
+
+        // checks if there's a match
+        if ($records === 1){
+            $row = pg_fetch_array($result, 0, PGSQL_NUM);
+
+            $this->setUserId($row["user_id"]);
+            $this->setPassword($row["password"]);
+            $this->setEmailAddress($row["email_address"]);
+            $this->setUserType($row["user_type"]);
+            $this->setEnroldate($row["enrol_date"]);
+            $this->setLastAccess($row["last_access"]);
+
+            // remove just for debugging.
+            echo $this->getEmailAddress();
+
+        }else{
+            return false;
+        }
+
+    }
+
+    public function GetAllUsers(){
+
+    }
+
+    public function AddUser($email, $password){
+
+    }
+
+    public function UpdateUser($email, $password, $userType){
+
+    }
+
+    public function DeleteUser($userId){
+
+    }
+
+
+}
